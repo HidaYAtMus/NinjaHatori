d3ac3914ce79c965b5b5ac6a290bfb0d17e90951
HP <HP@R1o-mu5>
2018-10-17 19:57:16 +0700

    menambahklan atribut pada class item dan membuat method membuat class DiscountItem

--- src/id/ac/polinema/midterm/Item.java
+++ src/id/ac/polinema/midterm/Item.java
@@ -11,5 +11,20 @@ package id.ac.polinema.midterm;
  * @author dhanifudin
  */
 public class Item {
+    protected String name;
+    protected float price;
+    protected int amount;
 
+    public Item() {
+    }
+
+    public Item(String name, float price, int amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+    
+    public float getTotalPrice(){
+        return (amount * price)-( price * amount);
+    }
 }
